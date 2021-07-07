6fb62382651c57567a6ad7b79884ba1646d3a465
David Öhman <dohman_92@hotmail.com>
2021-07-07 13:16:04 +0200

    Add LocationViewModelTest

---
+++ android/CodeTest/app/src/test/java/com/codebase/main/viewmodels/LocationViewModelTest.kt
@@ -0,0 +1,24 @@
+package com.codebase.main.viewmodels
+
+import com.codetest.main.api.models.LocationRequest
+import com.codetest.main.viewmodels.LocationViewModel
+import com.nhaarman.mockito_kotlin.mock
+import org.junit.Test
+
+class LocationViewModelTest {
+    private val viewModel = LocationViewModel(locationRepo = mock())
+
+    @Test
+    fun `Unrealistically cold temperature should throw Exception`() {
+        viewModel
+            .postLocation(
+                LocationRequest(
+                    name = "Very cold place",
+                    temperature = "-300",
+                    status = "SNOW_CLOUD"
+                )
+            )
+            .test()
+            .assertFailure(Exception::class.java)
+    }
+}
